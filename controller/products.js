f35906f77d9beac5f7415434fd4d1c688f29db08
Sasi chandan <bhargavsasichandan.com>
2024-09-16 23:50:54 +0530

    latest

--- controller/products.js
+++ controller/products.js
@@ -2,10 +2,13 @@ const User= require("../model/user_model")
 
 // const name=contact.name;
 // const mail=contact.mail;
-const getAllProducts = async(req,res)=>{
-    
-    const data = await User.find();
-    res.status(200).json({ data});
-    
-}
+const getAllProducts = async (req, res) => {
+  try {
+    const data = await User.find(); // Fetch data from database
+    res.status(200).json({ data }); // Send data as JSON response
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch products" }); // Error handling
+  }
+};
+
 module.exports=getAllProducts
