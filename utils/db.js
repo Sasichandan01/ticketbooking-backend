45ac83258c9c2ae0a3655e0ee35e282acc6426c7
Sasi chandan <bhargavsasichandan.com>
2024-09-20 09:42:03 +0530

    latest

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
  //const URI = "mongodb://127.0.0.1:27017/ticket";
 const URI =
- process.env.MONGODB_URL;
+ process.env.MONGODB_URI;
 const connectDb=async()=>{
     try{
         await mongoose.connect(URI);
